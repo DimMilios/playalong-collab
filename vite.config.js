d4ac2e3200a86e2bfb4bd28c67261664851980e2
Dimitris Milios <dmilios@musicolab.hmu.gr>
2023-04-10 11:50:17 +0300

    fix: production build dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/apprepository/',
-  build: { sourcemap: true }
+  base: '/apprepository/playalong-collab/',
+  build: { sourcemap: true },
   server: { port: 5001 },
 });
